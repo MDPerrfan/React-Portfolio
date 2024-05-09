d4e4332e62801e06e1b9183177cc66b82ea9fc41
Mohammed Parves <mdperrfan@gmail.com>
2024-05-09 19:27:45 +0600

    Updated Intro text

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,27 +19,21 @@ function Home2() {
               LET ME <span className="coral"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I'm passionate about programming, specializing in web development.
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="coral"> C++, Javascript and Go. </b>
+                <b className="coral">  HTML, CSS, JavaScript and React.js </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              I also possess skills in &nbsp;
               <i>
-                <b className="coral">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="coral">
-                  Blockchain.
-                </b>
+                <b className="coral">C, PHP, and MySQL </b> 
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="coral">Node.js</b> and
+              I'm working to develop my skilss on <b className="coral">Node.js</b> and
               <i>
                 <b className="coral">
                   {" "}
